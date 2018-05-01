a678cd3a0d1be71681cd476b0dcc1c8452379f52
Cassandra Afseth <cafseth2@Illinois.edu>
2018-04-30 23:04:34 -0500

    second

--- MainActivity.java
+++ MainActivity.java
@@ -2,12 +2,21 @@ package com.example.caafs.notetaker;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+    private ListView list;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        list = (ListView)findViewById(R.id.listview);
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
     }
 }
